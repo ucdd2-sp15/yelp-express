1af48334684924283648dc7df6f8c9b68a50eb9f
BrianNewsom <brian.newsom@colorado.edu>
2015-02-03 16:18:37 -0700

    Revert "Implement search 50/50"
    This reverts commit fc23fba41b9377b16f3ee5c300d22523afdaf590.

--- search.js
+++ search.js
@@ -2,7 +2,6 @@ var doctors = require('./data/doctors.json')
 var restaurants = require('./data/restaurants.json')
 var users = require('./data/users.json')
 var tips = require('./data/tips.json')
-var _ = require('lodash');
 
 module.exports = function(app) {
 
@@ -13,9 +12,10 @@ module.exports = function(app) {
     app.get('/search/restaurants/name/has/:keyword', function(req, res) {
         var keyword = req.params.keyword
 
-        var rs = _.filter(restaurants, function(r){
-            return _.contains(r.name,keyword);
-        })
+
+        // TODO: lookup restaurants whose names contain the given keyword
+        var rs = [restaurants[6], restaurants[10]] // hardcoded for 'Pizza'
+
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
@@ -24,11 +24,8 @@ module.exports = function(app) {
     app.get('/search/restaurants/good/for/:x', function(req, res) {
         var x = req.params.x
 
-        var rs = _.filter(restaurants, function(r){
-            if(r.attributes['Good For']){
-                return r['attributes']['Good For'][x];
-            }
-        })
+        // TODO: lookup restaurants good for  :x
+        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -38,44 +35,32 @@ module.exports = function(app) {
     app.get('/search/restaurants/ambience/is/:x', function(req, res) {
         var x = req.params.x
 
-        var rs = _.filter(restaurants, function(r){
-            if(r.attributes['Ambience']){
-                console.log('yay');
-                return r.attributes.Ambience[x];
-            }
-        })
+        // TODO: lookup restaurants has ambience of :x
+        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
 
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
-    })
+    })    
 
     app.get('/search/restaurants/category/is/:x', function(req, res) {
         var x = req.params.x
-        // Fix misalign of fast-food
-        if (x == 'Fast-Food') x = 'Fast Food';
-        var rs = _.filter(restaurants, function(r){
-            if (_.contains(r.categories, x)) return true;
-        })
+
+        // TODO: lookup restaurants belonging to category :x
+        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
 
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
-    })
+    })    
 
 
     app.get('/search/restaurants/stars/:relationship/:number', function(req, res) {
         var number = req.params.number
         var relationship = req.params.relationship
 
-        var rs = _.filter(restaurants, function(r){
-            if (relationship == 'above'){
-                if(r.stars >= number) return true;
-            } else if (relationship == 'below'){
-                if(r.stars <= number) return true;
-            }
-            return false;
-        })
+        // TODO: lookup restaurants with starts higher or lower than :number
+        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -83,45 +68,20 @@ module.exports = function(app) {
     })
 
     app.get('/search/restaurants/q', function(req, res) {
-
+                
         var name = req.query.name
         var minStars = req.query.minStars
         var category = req.query.category
-        var ambience = req.query.ambience
+        var ambience = req.query.ambience    
+        
+        console.log('req.query: ', req.query)    
+        
+        // // TODO: lookup restaurants with the given query parameters
+        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
 
-        console.log('req.query: ', req.query)
-
-        var rs = restaurants;
-
-        if (name){
-            var rs = _.filter(rs, function(r){
-                if (_.contains(r.name,name)) return true;
-            })
-        }
-
-        if (minStars){
-            var rs = _.filter(rs, function(r){
-                if(r.stars >= minStars) return true;
-            })
-        }
-
-        if(category){
-            var rs = _.filter(rs, function(r){
-                if(_.contains(r.categories, category)) return true;
-            })
-        }
-
-        if(ambience){
-            var rs = _.filter(rs, function(r){
-                if(r.attributes['Ambience']){
-                    console.log('yay');
-                    return r.attributes.Ambience[ambience];
-                }
-            })
-        }
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
-    })
+    })    
 
 }
