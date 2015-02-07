5eea4c56080a3d08326ca8df8835e3ef58835808
melissab147 <melissa.j.bica@gmail.com>
2015-02-07 12:49:27 -0700

    Search.js completed, 50/50

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ var doctors = require('./data/doctors.json')
 var restaurants = require('./data/restaurants.json')
 var users = require('./data/users.json')
 var tips = require('./data/tips.json')
-var _ = require('lodash');
+var _ = require('lodash')
 
 module.exports = function(app) {
 
@@ -11,24 +11,31 @@ module.exports = function(app) {
     })
 
     app.get('/search/restaurants/name/has/:keyword', function(req, res) {
-        var keyword = req.params.keyword
+        var keyword = req.params.keyword;
 
-        var rs = _.filter(restaurants, function(r){
-            return _.contains(r.name,keyword);
-        })
+        var rs = _.filter(restaurants, function(n){
+            if(n.name != undefined)
+                return _.includes(n.name, keyword);
+        });
+
+        // _.filter returns all items in collection that return true for the function
+        // _.includes returns true if keyword is in restaurant name
+
         res.render('listRestaurants.jade', {
             restaurants: rs
-        })
+        });
     })
 
     app.get('/search/restaurants/good/for/:x', function(req, res) {
         var x = req.params.x
 
-        var rs = _.filter(restaurants, function(r){
-            if(r.attributes['Good For']){
-                return r['attributes']['Good For'][x];
+        var rs = _.filter(restaurants, function(n){
+            if(n.attributes["Good For"] != undefined){
+                // if(n.attributes["Good For"][x] == true) //test
+                //     console.log(n.name);
+                return (n.attributes["Good For"][x] == true);
             }
-        })
+        });        
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -38,43 +45,54 @@ module.exports = function(app) {
     app.get('/search/restaurants/ambience/is/:x', function(req, res) {
         var x = req.params.x
 
-        var rs = _.filter(restaurants, function(r){
-            if(r.attributes['Ambience']){
-                return r.attributes.Ambience[x];
+        var rs = _.filter(restaurants, function(n){
+            if(n.attributes["Ambience"] != undefined){
+                // if(n.attributes["Ambience"][x] == true) //test
+                //     console.log(n.name);
+                return (n.attributes["Ambience"][x] == true);
             }
-        })
+        });    
 
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
-    })
+    })    
 
     app.get('/search/restaurants/category/is/:x', function(req, res) {
         var x = req.params.x
-        // Fix misalign of fast-food
-        if (x == 'Fast-Food') x = 'Fast Food';
-        var rs = _.filter(restaurants, function(r){
-            if (_.contains(r.categories, x)) return true;
-        })
+
+        if(x == 'Fast-Food')
+            x = 'Fast Food';
+
+        var rs = _.filter(restaurants, function(n){
+            if(n.categories != undefined){
+                // if(_.includes(n.categories, x)) //test
+                //     console.log(n.name);
+                return (_.includes(n.categories, x));
+            }
+        });    
 
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
-    })
-
+    })    
 
     app.get('/search/restaurants/stars/:relationship/:number', function(req, res) {
         var number = req.params.number
         var relationship = req.params.relationship
 
-        var rs = _.filter(restaurants, function(r){
-            if (relationship == 'above'){
-                if(r.stars >= number) return true;
-            } else if (relationship == 'below'){
-                if(r.stars <= number) return true;
+        var rs = _.filter(restaurants, function(n){
+            if(n.stars != undefined){
+                if(relationship == 'below'){
+                    if(n.stars <= number)
+                        return true;
+                }
+                if(relationship == 'above'){
+                    if(n.stars >= number)
+                        return true;                
+                }
             }
-            return false;
-        })
+        });           
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -82,41 +100,50 @@ module.exports = function(app) {
     })
 
     app.get('/search/restaurants/q', function(req, res) {
-
         var name = req.query.name
         var minStars = req.query.minStars
         var category = req.query.category
         var ambience = req.query.ambience
 
         console.log('req.query: ', req.query)
+        console.log(category)
 
+        // Start out with all restaurants, then go through and
+        // filter out the list for each provided query term
         var rs = restaurants;
 
         if (name){
-            var rs = _.filter(rs, function(r){
-                if (_.contains(r.name,name)) return true;
+            rs = _.filter(rs, function(n){
+                if(n.name != undefined)
+                    return(_.includes(n.name, name));
             })
         }
 
-        if (minStars){
-            var rs = _.filter(rs, function(r){
-                if(r.stars >= minStars) return true;
-            })
+        if(minStars){
+            rs = _.filter(rs, function(n){
+                if(n.stars != undefined){
+                    if(n.stars >= minStars)
+                        return true;                
+                }
+            });             
         }
 
         if(category){
-            var rs = _.filter(rs, function(r){
-                if(_.contains(r.categories, category)) return true;
-            })
+            rs = _.filter(rs, function(n){
+                if(n.categories != undefined){
+                    return (_.includes(n.categories, category));
+                }
+            });               
         }
 
         if(ambience){
-            var rs = _.filter(rs, function(r){
-                if(r.attributes['Ambience']){
-                    return r.attributes.Ambience[ambience];
+            rs = _.filter(rs, function(n){
+                if(n.attributes["Ambience"] != undefined){
+                    return (n.attributes["Ambience"][ambience] == true);
                 }
-            })
+            });               
         }
+
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
