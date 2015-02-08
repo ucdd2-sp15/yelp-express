18143b6e3cbf9d8aced185bdeea0a050e6a1898b
Abebe <abta3355@colorado.edu>
2015-02-08 16:30:21 -0700

    50/50 points

--- search.js
+++ search.js
@@ -54,6 +54,7 @@ return chr.attributes['Good For'][x]};});
 
         // TODO: lookup restaurants belonging to category :x
         var rs = _.filter(restaurants, function(chr){for( cat in chr.categories){ console.log(chr.categories[cat])
+			if(x.indexOf('-')>-1){x=x.replace(/-/gi, " ")}
 				if (chr.categories[cat] == x){
 				return true} ;}});
 		console.log(rs)
@@ -68,9 +69,13 @@ return chr.attributes['Good For'][x]};});
         var relationship = req.params.relationship
 
         // TODO: lookup restaurants with starts higher or lower than :number
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
-
-        res.render('listRestaurants.jade', {
+        var rs = _.filter(restaurants, function(chr){
+		if (relationship=='above')
+			{return chr.stars >=number}
+		else if (relationship == 'below')
+			{return chr.stars <= number}
+		});
+		res.render('listRestaurants.jade', {
             restaurants: rs
         })
     })
@@ -85,8 +90,22 @@ return chr.attributes['Good For'][x]};});
         console.log('req.query: ', req.query)    
         
         // // TODO: lookup restaurants with the given query parameters
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
-
+        var rs = _.filter(restaurants, function(chr){
+			if (category!=undefined &&category.indexOf('-')>-1)
+				{category=category.replace(/+/gi, " ")}
+			if(name != undefined && minStars ==undefined && category == undefined && ambience == undefined)
+				{return chr.name.indexOf(name) > -1;	}
+			if(name == undefined && minStars !=undefined && category == undefined && ambience == undefined)
+                {return chr.stars >= minStars}
+			if(name != undefined && minStars !=undefined && category == undefined && ambience == undefined)
+                {return (chr.stars>= minStars && chr.name.indexOf(name)> -1)}
+			if(name == undefined && minStars !=undefined && category != undefined && ambience == undefined)
+                {for( cat in chr.categories){ console.log(chr.categories[cat])
+                if (chr.categories[cat] == category)return (chr.stars >= minStars)}}
+			if(name == undefined && minStars !=undefined && category == undefined && ambience != undefined)if(chr.attributes.Ambience != undefined){
+                {console.log("ambi=="+ chr.attributes.Ambience[ambience])
+				return chr.stars >= minStars && chr.attributes.Ambience[ambience]}}
+		});
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
