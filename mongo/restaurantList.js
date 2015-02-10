ceaa5dd752188ba678514517c326ec16729ce926
Andrea Sassu <andrea.sassu@colorado.edu>
2015-02-10 12:57:10 -0700

    Merging and allowing mongoDB to run restaurants and doctors

--- mongo/restaurantList.js
+++ mongo/restaurantList.js
@@ -1,5 +1,26 @@
 module.exports = function(app) {
-
-    // TODO
     
+    app.get('/list/restaurants', function(req, res) {
+
+        // get the business collection
+        var business = app.db.get('business')
+
+        // compose a query to look up rests whose 'categories' field contains the word 'Restaurants'
+        var q = {
+            'categories': {
+                $in: ['Restaurants']
+            }
+        }
+
+        // execute the query to find those matched limiting to 20
+        business.find(q, {
+            limit: 20
+        }, function(err, rests) {
+
+            res.render('listRestaurants.jade', {
+                restaurants: rests
+            })
+        })
+
+    })
 }
