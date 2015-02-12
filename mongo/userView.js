6ebedc37c5e96e9b7e01629b5df7fc6c72ec50aa
Jake Smith <jakehockey10@gmail.com>
2015-02-11 18:04:29 -0700

    implements restaurants and users

--- mongo/userView.js
+++ mongo/userView.js
@@ -1,5 +1,21 @@
 module.exports = function(app) {
 
-    // TODO
+    app.get('/view/user/:user_id', function(req, res) {
+        console.log("hi");
+        // get the business collection
+        var users = app.db.get('user')
+
+         var q = {
+            'user_id': req.params.user_id            
+        }
+
+        var user = users.findOne(q, function(err, item) {
+            console.log(item);
+            res.render('viewUser.jade', {
+                user: item
+            })
+        })
+
+    })
     
 }
